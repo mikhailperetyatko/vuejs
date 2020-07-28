e7b34813fa9fe6ac40d55bb3c2a4673914de1e06
Mikhail Peretyatko <mihanya@list.ru>
2020-07-29 00:18:33 +0300

    В структуру App.vue добавлены фильтры. Добавлены категории и они внесены в перечень товаров. В товарах добавлены доступные цены и изменен шаблон выбора цвета из доступных цветов. Создан компонент ProductFilter

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-<section class="catalog">
-  <ProductList :products="products" />
-  <BasePagination :current-page="page" :per-page="productsPerPage" :amount="productsAmount" v-model="page" />
-</section>
+<div class="content__catalog">
+  <ProductFilter />
+  <section class="catalog">
+    <ProductList :products="products" />
+    <BasePagination :current-page="page" :per-page="productsPerPage" :amount="productsAmount" v-model="page" />
+  </section>
+</div>
 </template>
 
 <script>
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
 import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
   data() {
     return {
       page: 1,
-      productsPerPage: 3,
+      productsPerPage: 6,
     };
   },
   computed: {
@@ -30,6 +34,7 @@ export default {
   components: {
     ProductList,
     BasePagination,
+    ProductFilter,
   },
 };
 </script>
--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -12,24 +12,10 @@
     {{ product.price.toLocaleString() }} ₽
   </span>
   <ul class="colors colors--black">
-    <li class="colors__item">
+    <li class="colors__item" v-for="color in product.colors" :key="color">
       <label class="colors__label">
-        <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-        <span class="colors__value" style="background-color: #73B6EA;">
-        </span>
-      </label>
-    </li>
-    <li class="colors__item">
-      <label class="colors__label">
-        <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-        <span class="colors__value" style="background-color: #8BE000;">
-        </span>
-      </label>
-    </li>
-    <li class="colors__item">
-      <label class="colors__label">
-        <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-        <span class="colors__value" style="background-color: #222;">
+        <input class="colors__radio sr-only" type="radio" :value="color" v-model="currentColor">
+        <span class="colors__value" :style="'background-color: ' + color">
         </span>
       </label>
     </li>
@@ -42,5 +28,10 @@ export default {
   props: [
     'product',
   ],
+  data() {
+    return {
+      currentColor: this.product.colors[0],
+    };
+  },
 };
 </script>
